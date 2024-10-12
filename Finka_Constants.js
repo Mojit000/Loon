b6fe62e3aa5ec2eea88c986fd2374d6a20a35f66
Mojit0 <yan199219dang@gmail.com>
2024-10-13 00:08:37 +0800

    Update Finka_Constants.js

--- Finka_Constants.js
+++ Finka_Constants.js
@@ -3,10 +3,10 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("/constants/ios")) {
+if (url.includes("/feed/v5")) {
     if (obj.data && obj.data.list) {
         // 过滤掉动态为彩虹A+会员推荐
-        obj.data.list = obj.data.list.filter(constant => constant.type !== "PostSpecialExposure");
+        obj.data.list = obj.data.list.filter(constant => constant.hasOwnProperty("description"));
     }
 }
 
