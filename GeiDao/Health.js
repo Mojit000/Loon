f2b798c1b5e9e2a81873b5f3a1c8a5e375a11d10
Mojit0 <yan199219dang@gmail.com>
2024-10-13 14:53:07 +0800

    Update Health.js

--- GeiDao/Health.js
+++ GeiDao/Health.js
@@ -1,1 +1,14 @@
+// 2024-10-12 21:02:25
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
 
+if (url.includes("/give-health-api/home")) {
+  if (obj.data) {
+    // 精简“我的”
+    obj.data.aggregations = null
+    obj.data.banners = null
+  }
+}
+
+$done({ body: JSON.stringify(obj) });
