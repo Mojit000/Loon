fbf52f5732188ec979bb627f949a9819bf5e5cc5
Mojit0 <yan199219dang@gmail.com>
2024-10-13 14:24:14 +0800

    Update Hot_Market.js

--- GeiDao/Hot_Market.js
+++ GeiDao/Hot_Market.js
@@ -1,5 +1,14 @@
+// 2024-10-12 21:02:25
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
 
+if (url.includes("/api/v1/home/display_zone?type=2")) {
+    if (obj.data && obj.data.sections) {
+        // 过滤掉tab为"语音房"的tab
+        obj.data.sections = obj.data.sections.filter(item => item.biz_type !== "banners");
+    }
+}
 
-^https:\/\/gapi\.guanaitong\.com\/api\/v1\/home\/display_zone\?type=2 url reject-dict
+$done({ body: JSON.stringify(obj) });
 
-hostname = gapi.guanaitong.com
