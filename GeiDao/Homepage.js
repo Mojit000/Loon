08d9125acaf13803c53d5f73dd6953a5fe015387
Mojit0 <yan199219dang@gmail.com>
2024-10-13 13:48:01 +0800

    Update Homepage.js

--- GeiDao/Homepage.js
+++ GeiDao/Homepage.js
@@ -7,8 +7,18 @@ const lite_floors = [
   {
     "key": "default_market",
     "market": {
+      "hot_act": {
+            "title": "肯德基",
+            "img_url": "http://img1.guanaitong.com/grus-gfs/product/app-mgr/by-days/2020-06-23/a7badf82a51f4e93dafe884507ee3d3d.png",
+            "link_url": "gatgive://page.gat/web?url=https%3A%2F%2Ft.guanaitong.com%2FjxeMu0dp"
+          },
       "title": "热门商城",
       "more_link_title": "查看全部",
+      "topic_act": {
+            "title": "奈雪の茶",
+            "img_url": "https://file.guanaitong.com/grus-gfs/product/app-mgr/by-days/2022-10-28/f7d9982c7afdb77626e736c54bef8075.png",
+            "link_url": "gatgive://page.gat/web?url=https%3A%2F%2Ft.guanaitong.com%2F6JETXjGg"
+          },
       "more_link_url": "https://m.igeidao.com/home/display_zone?type=2",
       "values": [
         {
