46d90620b18192fefb8f5294d6517562aafa82fe
Mojit0 <yan199219dang@gmail.com>
2024-10-13 08:55:10 +0800

    Update Blued_lite.js

--- Blued/Blued_lite.js
+++ Blued/Blued_lite.js
@@ -6,7 +6,7 @@ let obj = JSON.parse($response.body);
 if (url.includes("/ticktocks/users")) {
     if (obj.data) {
         // 体重大于75kg，年龄在28-40岁通讯录的动态
-        obj.data.list = obj.data.filter(item => item.weight < 75 && item.age > 28 && item.age < 40);
+        obj.data.list = obj.data.filter(item => item.weight > 75 && (item.age < 28 || item.age > 40));
     }
 }
 
