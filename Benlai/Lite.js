86acdf9eee5f8df74e90479c8e04e6d984578fc1
Mojit0 <yan199219dang@gmail.com>
2024-10-15 14:57:45 +0800

    Update Lite.js

--- Benlai/Lite.js
+++ Benlai/Lite.js
@@ -1,2 +1,15 @@
-https://api.benlai.com/v5/HomePage/Category?
+//https://api.benlai.com/v5/HomePage/Category?
 
+// 2024-10-12 21:02:25
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (url.includes("/HomePage/Category")) {
+  if (obj.data && obj.data.suspension) {
+    // 精简首页：悬浮按钮
+    obj.data.suspension = null
+  }
+}
+
+$done({ body: JSON.stringify(obj) });
