631d9e0de74f07e0993ee013b259f7f50d30f9e0
Mojit0 <yan199219dang@gmail.com>
2024-10-15 18:39:06 +0800

    Update Lite.js

--- Benlai/Lite.js
+++ Benlai/Lite.js
@@ -28,6 +28,8 @@ if (url.includes("/AppSupport/BusinessConfig?areaId")) {
     // 精简首页：Tab
     obj.data.keyWorld = ""
     obj.data.bottom.buttons = obj.data.bottom.buttons.filter(button => button.name !== "C位已登录")
+    obj.data.bottom.buttons[2].siteNo = 3
+    obj.data.bottom.buttons[3].siteNo = 4
   }
 }
 
