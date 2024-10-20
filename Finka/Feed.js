f61d8819f77d7bb4db31304ea10226c97eba1e1b
Mojit0 <yan199219dang@gmail.com>
2024-10-20 10:21:29 +0800

    Update Feed.js

--- Finka/Feed.js
+++ Finka/Feed.js
@@ -5,7 +5,7 @@ let obj = JSON.parse($response.body);
 
 if (url.includes("/feed/v5")) {
     if (obj.data && obj.data.list) {
-        // 过滤掉动态为彩虹A+会员推荐
+        // 过滤掉动态为彩虹A+会员推荐、官方企划、动态推广
         obj.data.list = obj.data.list.filter(item => !(item.hasOwnProperty("postBoostInfo")));
         obj.data.list = obj.data.list.filter(item => item.type !== "PostSpecialExposure");
         obj.data.list = obj.data.list.filter(item => item.label !== "特别企划");
